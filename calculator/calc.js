9ea97ae8a341a985ff0ec49b66ff8d2043481783
Paradox Programming <nathaniel.fargo@gmail.com>
2019-04-15 15:33:34 -0600

    Made some changes and made more code work

--- calculator/calc.js
+++ calculator/calc.js
@@ -1,27 +1,51 @@
-var money = 0;
-var submit = document.querySelector("button");
-var output = document.getElementById("output");
-var splits = [
-  {
-    name: "savings",
-    percent: 50,
-  },
-  {
-    name: "charity",
-    percent: 10,
-  },
-  {
-    name: "spending",
-    percent: 40
-  }
-];
-submit.addEventListener("click",function(){
-    money = document.getElementById("amount").value;
-    var outputHTML = 'Here are your results<br>';
+//Calculator Variables
+var calc = {
+    money: 0,
+    splits: [
+      {
+        name: "savings",
+        percent: 50,
+      },
+      {
+        name: "charity",
+        percent: 10,
+      },
+      {
+        name: "spending",
+        percent: 40
+      }
+  ],
+}
+//HTML Objects
+var elements = {
+    submit: document.getElementById("submit"),
+    output: document.getElementById("output"),
+    edit: document.getElementById("edit"),
+    editOutput: document.getElementById("editCalcOutput"),
+}
+//Modifying functions
+var modifiers = {
+    upFirstLetter: function(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+}
+//Event listeners
+elements.submit.addEventListener("click", function(){
+    calc.money = document.getElementById("amount").value;
+    var outputHTML = 'Here are your results';
 
-    for(var i = 0; i < splits.length; i ++){
-      outputHTML+=splits[i].name+": "+splits[i].percent/100*money+"<br>";
+    for(var i = 0; i < calc.splits.length; i ++){
+        var splitWay = calc.splits[i];
+        var name = modifiers.upFirstLetter(splitWay.name)
+        var amount = splitWay.percent/100 * calc.money;
+
+        outputHTML+="<br>";
+        outputHTML+= name + ": " + Math.floor(amount*100)/100;
     }
 
-    output.innerHTML = outputHTML;
+    elements.output.innerHTML = outputHTML;
 });
+function createInputArea(){
+    
+}
+elements.edit.addEventListener("click", createInputArea)
