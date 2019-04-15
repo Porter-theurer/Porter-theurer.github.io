ac2ac8e0f6f85c5ec094845c0dba673578f3d498
Porter-theurer <48871227+Porter-theurer@users.noreply.github.com>
2019-04-15 14:45:01 -0600

    new stuff

--- calculator/calc.js
+++ calculator/calc.js
@@ -1,1 +1,27 @@
-var money = document.getElementById("amount").value();
+var money = 0;
+var submit = document.querySelector("button");
+var output = document.getElementById("output");
+var splits = [
+  {
+    name: "savings",
+    percent: 50,
+  },
+  {
+    name: "charity",
+    percent: 10,
+  },
+  {
+    name: "spending",
+    percent: 40
+  }
+];
+submit.addEventListener("click",function(){
+    money = document.getElementById("amount").value;
+    var outputHTML = 'Here are your results<br>';
+
+    for(var i = 0; i < splits.length; i ++){
+      outputHTML+=splits[i].name+": "+splits[i].percent/100*money+"<br>";
+    }
+
+    output.innerHTML = outputHTML;
+});
