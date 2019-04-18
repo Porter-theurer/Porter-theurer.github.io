77a8df785c515feaf0e4ccbc511a0e3e727ce326
Porter-theurer <48871227+Porter-theurer@users.noreply.github.com>
2019-04-18 14:42:13 -0600

    work

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ function makeCalculator(){
   var c_type = prompt('What calculator would you like to make?')
   if (c_type === "SPLIT") {
     window.location.href = "calculator";
+  } else if ("NEEDED MON") {
+    window.location.href = "neededMon";
   } else {
     alert('invalid response')
   }
